c4df39299f93d1af498a2cb216c1d364dbc036d7
whidaeyoung <80248735+whidaeyoung@users.noreply.github.com>
2023-04-24 11:39:01 +0900

    DotNet C# Console Project Add

---
+++ DotNet/DotNet/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("C# 책 및 강의에서 사용되는 C# 데모 코드 모음 프로젝트");
+        }
+    }
+}
