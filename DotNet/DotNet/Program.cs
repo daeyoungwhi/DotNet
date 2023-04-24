7140bf36893079eb0eab5d8134a9ed6b79672af4
whidaeyoung <80248735+whidaeyoung@users.noreply.github.com>
2023-04-24 11:52:40 +0900

    GitHub URL 추가

--- DotNet/DotNet/Program.cs
+++ DotNet/DotNet/Program.cs
@@ -1,16 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DotNet
 {
     internal class Program
     {
         static void Main(string[] args)
-        {
+        { 
             Console.WriteLine("C# 책 및 강의에서 사용되는 C# 데모 코드 모음 프로젝트");
+            Console.WriteLine("https://github.com/daeyoungwhi/DotNet");
         }
     }
-}
+} 
